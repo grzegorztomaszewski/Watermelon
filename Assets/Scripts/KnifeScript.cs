63298fb208b815f621918638647ed422f8df2709
grzegorztomaszewski <aroxd3@gmail.com>
2018-12-30 19:52:03 +0100

    added second Script

---
+++ Assets/Scripts/KnifeScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class KnifeScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector2 throwForce;
+
+    //knife shouldn't be controlled by the player when it's inactive 
+    //(i.e. it already hit the log / another knife)
+    private bool isActive = true;
+
+    //for controlling physics
+    private Rigidbody2D rb;
+    //the collider attached to Knife
+    private BoxCollider2D knifeCollider;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        knifeCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        //this method of detecting input also works for touch
+        if (Input.GetMouseButtonDown(0) && isActive)
+        {
+            //"throwing" the knife
+            rb.AddForce(throwForce, ForceMode2D.Impulse);
+            //once the knife isn't stationary, we can apply gravity (it will not automatically fall down)
+            rb.gravityScale = 1;
+            //TODO: Decrement number of available knives
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //we don't even want to detect collisions when the knife isn't active
+        if (!isActive)
+            return;
+
+        //if the knife happens to be active (1st collision), deactivate it
+        isActive = false;
+
+        //collision with a log
+        if (collision.collider.tag == "Log")
+        {
+            //stop the knife
+            rb.velocity = new Vector2(0, 0);
+            //this will automatically inherit rotation of the new parent (log)
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            transform.SetParent(collision.collider.transform);
+
+            //move the collider away from the blade which is stuck in the log
+            knifeCollider.offset = new Vector2(knifeCollider.offset.x, -0.4f);
+            knifeCollider.size = new Vector2(knifeCollider.size.x, 1.2f);
+
+            //TODO: Spawn another knife
+        }
+        //collision with another knife
+        else if (collision.collider.tag == "Knife")
+        {
+            //start rapidly moving downwards
+            rb.velocity = new Vector2(rb.velocity.x, -2);
+            //TODO: Game Over
+        }
+    }
+}
