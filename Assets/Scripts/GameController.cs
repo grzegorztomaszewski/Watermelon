63f2011a269f8faa49c8fb69e7b08c3d0ea858cc
grzegorztomaszewski <aroxd3@gmail.com>
2019-07-21 18:07:53 +0200

    added new levels

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//this script cannot function properly without GameUI
+//let Unity know about it with this attribute
+[RequireComponent(typeof(GameUI))]
+public class GameController : MonoBehaviour
+{
+    public Slider sliderProgress;
+    public Text pointsText;
+    public int points;
+
+
+    //we can get this instance from other scripts very easily
+    public static GameController Instance { get; private set; }
+
+    [SerializeField]
+    private int knifeCount;
+
+    [Header("Knife Spawning")]
+    [SerializeField]
+    private Vector2 knifeSpawnPosition;
+    [SerializeField]
+    //this will be a prefab of the knife. You will create the prefab later.
+    private GameObject knifeObject;
+
+    //reference to the GameUI on GameController's game object
+    public GameUI GameUI { get; private set; }
+
+    private void Awake()
+    {
+        //simple kind of a singleton instance (we're only in 1 scene)
+        Instance = this;
+        GameUI = GetComponent<GameUI>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void Start()
+    {
+
+        pointsText.text = "Points: " + points;
+        //update the UI as soon as the game starts
+        GameUI.SetInitialDisplayedKnifeCount(knifeCount);
+        //also spawn the first knife
+        SpawnKnife();
+        
+    }
+
+    //this will be called from KnifeScript
+    public void OnSuccessfulKnifeHit()
+    {
+        points++;
+        pointsText.text = "Points: " + points;
+        sliderProgress.value++;
+
+        if (knifeCount > 0)
+        {
+            SpawnKnife();
+        }
+        else if (points == 8)
+        {
+            SceneManager.LoadScene("Level2");
+
+        }
+        else if (points == 16)
+        {
+            SceneManager.LoadScene("Level3");
+        }
+        else if (points == 24)
+        {
+            SceneManager.LoadScene("Level4");
+        }
+        else if (points == 32)
+        {
+            SceneManager.LoadScene("Level5");
+        }
+        else if (points == 40)
+        {
+            SceneManager.LoadScene("Level6");
+        }
+        else if (points == 48)
+        {
+            SceneManager.LoadScene("Level7");
+        }
+        else if (points == 56)
+        {
+            SceneManager.LoadScene("Level8");
+        }
+        else if (points == 64)
+        {
+            SceneManager.LoadScene("Level9");
+        }
+        else if (points == 72)
+        {
+            SceneManager.LoadScene("Level10");
+        }
+        else if (points == 80)
+        {
+            SceneManager.LoadScene("Level11");
+        }
+        else if (points == 88)
+        {
+            SceneManager.LoadScene("Level12");
+        }
+        else if (points == 96)
+        {
+            SceneManager.LoadScene("Level13");
+        }
+        else if (points == 104)
+        {
+            SceneManager.LoadScene("Level14");
+        }
+        else if (points == 112)
+        {
+            SceneManager.LoadScene("Level15");
+        }
+        else if (points == 120)
+        {
+            SceneManager.LoadScene("Level16");
+        }
+        else if (points == 128)
+        {
+            SceneManager.LoadScene("Level17");
+        }
+        else if (points == 136)
+        {
+            SceneManager.LoadScene("Level18");
+        }
+        else if (points == 144)
+        {
+            SceneManager.LoadScene("Level19");
+        }
+        else if (points == 152)
+        {
+            SceneManager.LoadScene("Level20");
+        }
+        else if (points == 160)
+        {
+            SceneManager.LoadScene("basic");
+        }
+    }
+
+    private void SpawnKnife()
+    {
+        knifeCount--;
+        Instantiate(knifeObject, knifeSpawnPosition, Quaternion.identity);
+    }
+
+    //the public method for starting game over
+    public void StartGameOverSequence(bool win)
+    {
+
+        StartCoroutine("GameOverSequenceCoroutine", win);
+    }
+
+    private IEnumerator GameOverSequenceCoroutine(bool win)
+    {
+        if (win)
+        {
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+        else
+        {
+            GameUI.ShowRestartButton();
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+        SceneManager.LoadScene("basic");
+    }
+    //public void PauseGame(bool On)
+    //{
+    //    if (On)
+    //    {
+    //        Time.timeScale = Mathf.Approximately(Time.timeScale, 0.0f) ? 1.0f : 0.0f;
+    //       // KnifeScript.instance.isActive = false;
+    //    }
+    //}
+
+}
---
+++ Assets/Scripts/LogRotation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LogRotation : MonoBehaviour
+{
+
+    [System.Serializable] //this will allow us to edit it in the editor
+    //a custom class representing a single rotation "element" of the log's rotation pattern
+    private class RotationElement
+    {
+        //to get rid of an obnoxious warning about these fields not being initialized
+#pragma warning disable 0649
+        public float Speed;
+        public float Duration;
+#pragma warning restore 0649
+    }
+
+    [SerializeField] //attribute making private fields editable in the Unity Editor
+    //the aforemention full rotation pattern of the log
+    private RotationElement[] rotationPattern;
+
+    //this will be set to the Wheel Joint 2D from the LogMotor object
+    private WheelJoint2D wheelJoint;
+    //something has to actually apply a force to the log through the Wheel Joint 2D
+    private JointMotor2D motor;
+
+    private void Awake()
+    {
+        //setting fields
+        wheelJoint = GetComponent<WheelJoint2D>();
+        motor = new JointMotor2D();
+        //starting an infinitely looping coroutine defined below right when this script loads (awakes)
+        StartCoroutine("PlayRotationPattern");
+    }
+
+    private IEnumerator PlayRotationPattern()
+    {
+        int rotationIndex = 0;
+        //infinite coroutine loop
+        while (true)
+        {
+            //working with physics, executing as if this was running in a FixedUpdate method
+            yield return new WaitForFixedUpdate();
+
+            motor.motorSpeed = rotationPattern[rotationIndex].Speed;
+            //hard coded 10000, feel free to experiment with other torques if you wish
+            motor.maxMotorTorque = 10000;
+            //set the updated motor to be the motor "sitting" on the Wheel Joint 2D
+            wheelJoint.motor = motor;
+
+            //let the motor do its thing for the specified duration
+            yield return new WaitForSecondsRealtime(rotationPattern[rotationIndex].Duration);
+            rotationIndex++;
+            //infinite loop through the rotationPattern
+            rotationIndex = rotationIndex < rotationPattern.Length ? rotationIndex : 0;
+        }
+    }
+}
